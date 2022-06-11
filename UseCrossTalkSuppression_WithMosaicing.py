931abe7fd1abdcc0d9531b2d02f0a3afcb5aa184
melanogaster <melanogaster@melbourne>
2022-06-11 16:00:27 +0200

    new templates, some parameter changes

--- UseCrossTalkSuppression_WithMosaicing.py
+++ UseCrossTalkSuppression_WithMosaicing.py
@@ -52,13 +52,14 @@ subsampling=2           # use only every N'th sample to save processing power
 maxFlyTemplateSize=1024  # 2048 use only the first n mosaic grains to speed up NMFdiag
 reverbFactorFFT=0.95;   # fraction of old loudness retained from last chunk --- 0: no reverb ... 0.9999: almost inifinite reverb
 
-mosaicLoudnessBoost=100;			# loudness multiplier to compensate for losses due to reverb normalization
+mosaicLoudnessBoost=120;			# war 100 loudness multiplier to compensate for losses due to reverb normalization
 startupWaitCycles = 500; # to allow components to wake up from standby, we play a short burst of sound first before beginning calibration
 #initialize synthesizer with fly waveform
-filenameFly = 'template/ZOOM0006_Tr12_excerpt.WAV'
-#filenameFly = 'template/samples_Birgit_5_2022_mixdown.wav'
+#filenameFly = 'template/ZOOM0006_Tr12_excerpt.WAV'
+filenameFly = 'template/template_melano.wav'
+#filenameFly = 'template/template_komposition_7.wav'
 melMatrixVoice= librosa.filters.mel(48000, paramSTFT['blockSize'],n_mels=32,fmax=10000)
-melMatrixFly= librosa.filters.mel(48000, paramSTFT['blockSize'],n_mels=32,fmax=10000)
+melMatrixFly= librosa.filters.mel(48000, paramSTFT['blockSize'],n_mels=32,fmax=1000)
 synthesizer=MosaicSynthesizer(paramSTFT, paramNMFdiag,melMatrixVoice,melMatrixFly,reverbFactorFFT,subsampling)
 synthesizer.prepareFlyInformation(filenameFly,maxFlyTemplateSize)
 
